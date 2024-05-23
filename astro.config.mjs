18f5f9c2822ab52cf70aee75a1baa82342135541
Diego Tolentino <tolerevo@hotmail.com>
2024-05-22 21:12:09 -0600

    mend

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://capable-dusk-c61e87.netlify.app"
+  site: "https://capable-dusk-c61e87.netlify.app",
+  integrations: [preact()]
 });
-
