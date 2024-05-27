82ecc39c47ac8c89c35b98e8a8a2a465b361c8a8
Diego Tolentino <tolerevo@hotmail.com>
2024-05-27 02:09:39 -0600

    disable spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://capable-dusk-c61e87.netlify.app",
-  integrations: [preact(), sentry(), spotlightjs()]
+  integrations: [preact()],
 });
