c8b63c5be0ddcdf413b7c01c5d580730ac9a522a
Diego Tolentino <tolerevo@hotmail.com>
2024-05-27 00:34:29 -0600

    add spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://capable-dusk-c61e87.netlify.app",
-  integrations: [preact()]
+  integrations: [preact(), sentry(), spotlightjs()]
 });
